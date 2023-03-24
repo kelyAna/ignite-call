4fc629bc4180826be8d8af2f17f1dc92703a88f5
kelyAna <kely.lopes@vtex.com.br>
2023-03-24 15:55:15 -0300

    feat: add users register

--- pages/index.page.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { Heading } from '@ignite-ui/react'
-
-export default function Home() {
-  return <Heading>Oi</Heading>
-}
--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import type { AppProps } from 'next/app'
-import { globalStyles } from '../styles/globals'
+import { globalStyles } from '../../styles/globals'
 
 globalStyles()
 
