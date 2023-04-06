2d11d3bfc3b4b6c8e9561ca067b0fcab5de20f37
kelyAna <kely.lopes@vtex.com.br>
2023-04-05 23:00:34 -0300

    feat: add connect calendar

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import type { AppProps } from 'next/app'
+import { SessionProvider } from 'next-auth/react'
 import { globalStyles } from '../../styles/globals'
 
 globalStyles()
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
+    <SessionProvider>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
